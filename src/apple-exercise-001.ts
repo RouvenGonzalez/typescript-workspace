bb5929e5fab97497d94aefa004013a84044eac4f
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-10-01 14:56:56 +0200

    version 2 with flat and filter

--- src/apple-exercise-001.ts
+++ src/apple-exercise-001.ts
@@ -3,7 +3,7 @@ export enum Fruit {
   Pear,
 }
 
-export function sortFruits(basket1: Fruit[], basket2: Fruit[]) {
+export function sortFruitsWithForEach(basket1: Fruit[], basket2: Fruit[]): Array<Fruit[]> {
   const apples = [];
   const pears = [];
   basket1.forEach((fruit) => {
@@ -17,6 +17,18 @@ export function sortFruits(basket1: Fruit[], basket2: Fruit[]) {
       apples.push(fruit);
     } else pears.push(fruit);
   });
+  const result = [apples, pears];
+  return result;
+}
+
+export function sortFruits(basket1: Fruit[], basket2: Fruit[]): Fruit[][] {
+  const fruits: Fruit[][] = [basket1, basket2];
+
+  const allfruits: Fruit[] = fruits.flat();
+
+  const apples = allfruits.filter((fruit) => fruit === Fruit.Apple);
+  const pears = allfruits.filter((fruit) => fruit === Fruit.Pear);
+
   const result = [apples, pears];
   return result;
 }
