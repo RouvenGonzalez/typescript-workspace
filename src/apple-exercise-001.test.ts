ca1ea30ea842bbb66cbd6c23ed6c4d5dbcb37580
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-10-01 14:06:28 +0200

    setup sortFruits exercise

---
+++ src/apple-exercise-001.test.ts
@@ -0,0 +1,17 @@
+import { Fruit, sortFruits } from './apple-exercise-001';
+
+describe('test sortFruits()', () => {
+  it('sort two arrays of Fruit types', () => {
+    // test data
+    const basket1 = [Fruit.Pear, Fruit.Apple, Fruit.Pear, Fruit.Apple];
+    const basket2 = [Fruit.Pear, Fruit.Apple, Fruit.Pear, Fruit.Apple];
+
+    // output
+    const result = [
+      [Fruit.Apple, Fruit.Apple, Fruit.Apple, Fruit.Apple],
+      [Fruit.Pear, Fruit.Pear, Fruit.Pear, Fruit.Pear],
+    ];
+
+    expect(sortFruits(basket1, basket2)).toEqual(result);
+  });
+});
---
+++ src/apple-exercise-001.ts
@@ -0,0 +1,6 @@
+export enum Fruit {
+  Apple,
+  Pear,
+}
+
+export function sortFruits(basket1: Fruit[], basket2: Fruit[]) {}
